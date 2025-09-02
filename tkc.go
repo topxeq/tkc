bab47b705ed59897011eb290bb7ec1604a6fdf3c
unknown <topxeq@163.com>
2025-09-02 10:46:33 +0800

    2025090201

--- tkc.go
+++ tkc.go
@@ -16824,6 +16824,20 @@ var JSONResponseToHTML = TKX.JSONResponseToHTML
 // max integer
 const MAX_INT = int(^uint(0) >> 1)
 
+func (pA *TK) CalDistanceOfLatLonToMeters(lat1, lon1, lat2, lon2 float64) float64 {
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = lat2 * math.Pi / 180 - lat1 * math.Pi / 180;
+    var dLon = lon2 * math.Pi / 180 - lon1 * math.Pi / 180;
+    var a = math.Sin(dLat/2) * math.Sin(dLat/2) +
+    math.Cos(lat1 * math.Pi / 180) * math.Cos(lat2 * math.Pi / 180) *
+    math.Sin(dLon/2) * math.Sin(dLon/2);
+    var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a));
+    var d = R * c;
+    return d * 1000; // meters
+}
+
+var CalDistanceOfLatLonToMeters = TKX.CalDistanceOfLatLonToMeters
+
 // Max 取多个数值的最大值，数据类型以第一个参数为准
 func (pA *TK) Max(nA ...interface{}) interface{} {
 	lenT := len(nA)
